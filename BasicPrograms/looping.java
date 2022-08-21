42df5a54980d0ec810b514214bafc9681c1a18d0
pradeep M <73049899+Pradeep10012001@users.noreply.github.com>
2022-08-21 21:28:35 +0530

    Add files via upload

---
+++ BasicPrograms/looping.java
@@ -0,0 +1,29 @@
+class looping
+{
+  public static void main(String args[])
+{
+  looping obj=new looping();
+  obj.loop();
+
+
+}
+
+public void loop()
+{
+ int i=1;
+while(i<=10)
+{
+System.out.println("hai");
+ i++;
+}
+
+}
+
+
+
+
+
+
+
+
+}
